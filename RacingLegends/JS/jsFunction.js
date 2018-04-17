08bc34b7fcb7b73ae7c0342a50f4d005de0c2244
Ricardo Reis <ricardo.galinha@hotmail.com>
2018-04-17 19:24:30 +0100

    Adição de bootstrap ao projeto

--- RacingLegends/JS/jsFunction.js
+++ RacingLegends/JS/jsFunction.js
@@ -1,1 +1,27 @@
-﻿
+﻿function getCategorias() {
+    var url = "http://ipt-ti2-racinglegends-api.eu-gb.mybluemix.net/api/v1/categories";
+
+    return fetch(url, { headers: { Accept: 'application/json' } })
+        .then(function (resposta) {
+            if (resposta.status === 200) {
+                return resposta.json();
+            } else {
+                return Promise.reject(new Error("Erro ao obter categorias"));
+            }
+        });
+
+}
+//função que recebe o ID de cada categoria para apresentar cada uma das imagens 
+function getImagemCat(categoraId) {
+    // quando se usa acentos graves(``) em vez de aspas("") utiliza-se ${} para adicionar informação dentro da string
+    var url = `http://ipt-ti2-racinglegends-api.eu-gb.mybluemix.net/api/v1/categories/${categoraId}/image`;
+    return url;
+}
+
+//função que recebe o id de cada categoria para apresentar os seus pilotos
+//É CAPAZ DE ESTAR MAL!!!!!!!
+function getPilotos(categoriaId) {
+    var url = `http://ipt-ti2-racinglegends-api.eu-gb.mybluemix.net/api/v1/categories/${categoriaId}/drivers`;
+    return url;
+
+}
--- RacingLegends/JS/jsLogic.js
+++ RacingLegends/JS/jsLogic.js
@@ -1,1 +1,67 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', function main() {
+    var xhr = new XMLHttpRequest();
+    /**
+     * @type {Element}
+     */
+
+    xhr.open('GET', 'https://ipt-ti2-racinglegends-api.eu-gb.mybluemix.net/api/v1/ui');
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+
+            init();
+        } else {
+            console.error('Erro', xhr.status);
+        }
+    };
+
+    xhr.onerror = function () {
+        console.error('Erro');
+    };
+
+    xhr.send();
+
+    function init() {
+        ecraCategorias();
+    }
+    // Ficheiro da aplicação
+
+    // 2) 
+    function mostraCategorias(categorias) {
+        for (var i = 0; i < categorias.length; i++) {
+            // Criar elementos HTML, colocá-los no body ou noutro lado qualquer.
+            var categoria = categorias[i];
+            var catGeral = document.getElementById("categorias");
+            var catTitulo = document.createElement("h2");
+            catTitulo.textContent = categoria.name;
+            catGeral.appendChild(catTitulo);
+
+            // apresentar cada descrição de cada categoria
+            var catDesc = document.createElement("h2");
+            catDesc.textContent = categoria.description;
+            catGeral.appendChild(catDesc);
+
+            //apresentar cada fotografia de cada categoria
+            var catImagem = document.createElement("img");
+            catImagem.setAttribute('src', getImagemCat(categoria.id));
+            catGeral.appendChild(catImagem);
+        }
+    }
+
+    function mostraPilotos() {
+
+    }
+
+    // 3)
+    function ecraCategorias() {
+        return getCategorias()
+            .then(function (categorias) {
+                mostraCategorias(categorias);
+            })
+            .catch(function (erro) {
+                console.error(erro);
+            });
+    }
+
+
+})
